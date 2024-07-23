495c2d46d20803c87a4d782c8d27b43405d7411f
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-07-23 17:04:13 +0900

    env: add actuator, open-telemetry support

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
             ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
                         .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
                         .anyExchange().authenticated())
